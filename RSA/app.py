00f267f103882817289589c01c7062a1f619072d
Witold Hajkowicz <witold.hajkowicz@student.put.poznan.pl>
2024-03-23 16:13:09 +0100

    some changes

--- RSA/app.py
+++ RSA/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/rsa', methods=['POST'])
 def rsa():
